e66248cca7aed894787a44b5815b35d46e38e4cc
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-04-03 22:15:22 +0100

    Use correct ASSET_PATH

--- lib/asset_path.py
+++ lib/asset_path.py
@@ -3,10 +3,10 @@ import os
 
 apps = os.listdir("/apps")  # noqa: PTH208
 path = ""
-ASSET_PATH = "apps//"
+ASSET_PATH = "apps/"
 
-if "github_user_tildagon_wotef" in apps:
-    ASSET_PATH = "/apps/github_user_tildagon_wotef/"
+if "pikesley_tildagon_wotef" in apps:
+    ASSET_PATH = "/apps/pikesley_tildagon_wotef/"
 
 if "wotef" in apps:
     ASSET_PATH = "apps/wotef/"
