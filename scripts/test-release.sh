21b1c7eb219e53eaf37d8893f40814bc361652eb
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-04-11 22:48:51 +0100

    Finetune movements

--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -8,3 +8,11 @@ mkdir -p ${OUTDIR}
 git tag ${TAG}
 
 git archive --format tar --prefix ${APP}/ ${TAG} > ${OUTDIR}/rc.tar
+
+cd ${OUTDIR}
+
+tar xvf rc.tar
+
+cd wotef
+python -m mpremote fs mkdir :/apps/wotef
+python -m mpremote fs cp -r * :/apps/wotef
